f0da788f7c17de727167806aa0f4c82b131e6cbc
Mohit Kumar <mkumar@tekion.com>
2021-03-21 12:20:27 +0530

    Interview portal server - Initial Commit

---
+++ src/test/java/com/maat/interviewportal/InterviewportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maat.interviewportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
